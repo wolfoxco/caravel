a330c5eb6cacfaef85b713275d0507b5766bda46
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-03-08 16:30:57 +0100

    :bug: Run bin in node and display help if no arguments

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 const program = require ('commander')
 
 const main = require ('../main.js')
@@ -7,5 +9,9 @@ program
 	.alias('m')
 	.description('Run migration files in Migration folder if necessary')
 	.action(() => main())
+
+if (!process.argv.slice(2).length) {
+  program.help()
+}
 
 program.parse(process.argv)
