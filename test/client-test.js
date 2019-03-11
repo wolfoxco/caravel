ded3800c3107c72bbad901f8674aa24d2a980a95
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-03-11 18:06:26 +0100

    :white_check_mark: Add first test to client

--- test/client-test.js
+++ test/client-test.js
@@ -1,14 +1,41 @@
-const should = require('chai').should() // eslint-disable-line
+const path = require('path')
 const fs = require('fs')
+const should = require('chai').should() // eslint-disable-line
+const expect = require('chai').expect
 
-const client = require('../src/client')
+const { create } = require('../src/client')
+
+const TMP_PATH = path.resolve('./tmp')
+const CONFIG_NAME = path.resolve(TMP_PATH, 'config.json')
+
+function createTmpDirectory() {
+  fs.mkdirSync(TMP_PATH)
+  fs.writeFileSync(CONFIG_NAME, JSON.stringify({
+    user: 'doctor',
+    database: 'caravel_test',
+    port: 5432,
+    host: 'localhost',
+  }))
+}
+
+function deleteTmpDirectory() {
+  fs.unlinkSync(CONFIG_NAME)
+  fs.rmdirSync(TMP_PATH)
+}
 
 describe('The database client', function() {
   context('During creation', function() {
-    before('Creating tmp directory', function() { fs.mkdirSync('./tmp') })
-    after('Deleting tmp directory', function() { fs.rmdirSync('./tmp') })
+    before('Creating tmp directory', createTmpDirectory)
+    after('Deleting tmp directory', deleteTmpDirectory)
 
-    it('should be able to read a config file')
+    it('should be able to read a config file', async function() {
+      const client = await create('./tmp/config.json')
+      client.user.should.equal('doctor')
+      client.database.should.equal('caravel_test')
+      client.port.should.equal(5432)
+      client.host.should.equal('localhost')
+      expect(client.password).to.be.null
+    })
     it('should be able to read a global DATABASE_URL variable')
     it('should be able to read a local DATABASE_URL variable in a .env file')
     it('should be able to read config from global environment')
