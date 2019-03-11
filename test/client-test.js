8711a92e52485e172439acd95240ac33e8e8b6e1
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-03-11 18:16:47 +0100

    :white_check_mark: Add tests for client

--- test/client-test.js
+++ test/client-test.js
@@ -7,20 +7,38 @@ const { create } = require('../src/client')
 
 const TMP_PATH = path.resolve('./tmp')
 const CONFIG_NAME = path.resolve(TMP_PATH, 'config.json')
+const ENV_FILE = path.resolve('./.env')
+
+const DB_USER = 'doctor'
+const DB_HOST = 'localhost'
+const DB_PORT = 5432
+const DB_NAME = 'caravel_test'
+
+function generateDatabaseURL() {
+  return `postgres://${DB_USER}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+}
 
 function createTmpDirectory() {
   fs.mkdirSync(TMP_PATH)
   fs.writeFileSync(CONFIG_NAME, JSON.stringify({
-    user: 'doctor',
-    database: 'caravel_test',
-    port: 5432,
-    host: 'localhost',
+    user: DB_USER,
+    database: DB_NAME,
+    port: DB_PORT,
+    host: DB_HOST,
   }))
 }
 
 function deleteTmpDirectory() {
   fs.unlinkSync(CONFIG_NAME)
   fs.rmdirSync(TMP_PATH)
+}
+
+function testClient(client) {
+  client.user.should.equal(DB_USER)
+  client.database.should.equal(DB_NAME)
+  client.port.should.equal(DB_PORT)
+  client.host.should.equal(DB_HOST)
+  expect(client.password).to.be.null
 }
 
 describe('The database client', function() {
@@ -30,14 +48,24 @@ describe('The database client', function() {
 
     it('should be able to read a config file', async function() {
       const client = await create('./tmp/config.json')
-      client.user.should.equal('doctor')
-      client.database.should.equal('caravel_test')
-      client.port.should.equal(5432)
-      client.host.should.equal('localhost')
-      expect(client.password).to.be.null
+      testClient(client)
     })
-    it('should be able to read a global DATABASE_URL variable')
-    it('should be able to read a local DATABASE_URL variable in a .env file')
+
+    it('should be able to read a global DATABASE_URL variable', async function() {
+      process.env.DATABASE_URL = generateDatabaseURL()
+      const client = await create()
+      testClient(client)
+      delete process.env.DATABASE_URL
+    })
+
+    it('should be able to read a local DATABASE_URL variable in a .env file', async function() {
+      fs.writeFileSync(ENV_FILE, `DATABASE_URL = ${generateDatabaseURL()}`)
+      const client = await create()
+      testClient(client)
+      delete process.env.DATABASE_URL
+      fs.unlinkSync(ENV_FILE)
+    })
+    
     it('should be able to read config from global environment')
     it('should crash if no config is given')
   })
