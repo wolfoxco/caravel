479ad67039a141654d211b750d6e2e501a91aeb8
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-03-12 16:52:01 +0100

    :recycle: Improve error handling

--- src/helpers.js
+++ src/helpers.js
@@ -14,7 +14,18 @@ const readFile = async path => {
   }
 }
 
-const readdir = util.promisify(fs.readdir)
+const readdir = async path => {
+  try {
+    return await util.promisify(fs.readdir)(path)
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      const newError = `${path} does not exists. Create the folder first.`
+      throw newError
+    } else {
+      throw error
+    }
+  }
+}
 
 const access = util.promisify(fs.access)
 
